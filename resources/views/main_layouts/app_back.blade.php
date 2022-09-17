2ef98bb9758df44ea10d3353b9dfa70346f45b47
rayhan133 <rayhanalshorif@gmail.com>
2022-09-18 02:16:14 +0600

    hello

--- resources/views/main_layouts/app_back.blade.php
+++ resources/views/main_layouts/app_back.blade.php
@@ -73,9 +73,7 @@
                 <a href="{{ url('/') }}" class="text-decoration-none">
                     <h1 class="m-0 display-5 font-weight-semi-bold">
                         {{-- <span class="text-primary font-weight-bold border px-3 mr-1">E</span> --}}
-                        @if($logo)
                         <img src="{{ asset($logo->logo) }}">
-                        @endif
                     </h1>
                 </a>
             </div>
@@ -94,10 +92,10 @@
             <div class="col-lg-3 col-6 text-right">
                 <a href="#" class="btn border">
                     <i class="fas fa-heart text-primary"></i>
-                    {{-- <span class="badge">@if (Session::get('LoggedUser')) @php
+                    <span class="badge">@if (Session::get('LoggedUser')) @php
                         $point = DB::table('visitors')->where('id',Session::get('LoggedUser')->id)->first();
                         echo $point->visitor_points;
-                        @endphp @endif</span> --}}
+                        @endphp @endif</span>
                 </a>
                 @if (Session::get('LoggedUser'))
                 <a href="{{ route('cart') }}" class="btn border">
@@ -106,9 +104,7 @@
                     $cart =
                     DB::table('cart')->where('visitor_id',Session::get('LoggedUser')->id)->where('cart_status',0)->get();
                     @endphp
-                    @if($cart)
                     <span class="badge">{{ $cart->count() }}</span>
-                    @endif
                 </a>
                 @else
                 <a href="{{ route('cart') }}" class="btn border">
@@ -149,7 +145,7 @@
                             $subcategories =
                             DB::table('subcategories')->where('category_id',$category->id)->where('sub_category_status','Enable')->where('sub_category_delete',1)->get();
                             @endphp
-                            @if ($subcategories)
+                            @if ($subcategories!=NULL)
                             @foreach ($subcategories as $subcategory)
                             <a href="{{ route('sub_category_product',[$category->id,$subcategory->id]) }}"
                                 class="dropdown-item">{{ $subcategory->sub_category_name }}</a>
@@ -223,9 +219,7 @@
 
                     <h1 class="mb-4 display-5 font-weight-semi-bold">
                         {{-- <span class="text-primary font-weight-bold border border-white px-3 mr-1">S</span> --}}
-                        @if($logo)
                         <img src="{{ asset($logo->logo) }}" alt="">
-                        @endif
                     </h1>
                 </a>
                 <p>Dolore erat dolor sit lorem vero amet. Sed sit lorem magna, ipsum no sit erat lorem et magna ipsum
